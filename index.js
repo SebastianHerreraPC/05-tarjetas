7806991fb61357288ba5e4f22a3e0c54ffeb2f2d
SebastianHerreraPc <sebehepe00@gmail.com>
2024-10-27 23:03:07 -0600

    marcado

---
+++ index.js
@@ -0,0 +1,8 @@
+class Card {
+  constructor(title, description, price, image) {
+    this._title = title;
+    this._description = description;
+    this._price = price;
+    this._image = image;
+  }
+}
